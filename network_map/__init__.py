0924735c361bfc70814cac7d69cf88e5d8a96ed1
Yannick Wernle <yannick@wernle.net>
2026-08-27 14:24:53 +0200

    Setup bare bones for plugin

---
+++ network_map/__init__.py
@@ -0,0 +1,12 @@
+from netbox.plugins import PluginConfig
+
+class NetworkMapConfig(PluginConfig):
+    name = "network_map"
+    verbose_name = "Netbox Network Map"
+    description = "A simple network map integrated into netbox"
+    version = '0.1.0'
+    base_url = 'networkmap'
+    min_version = '4.5.0'
+    max_version = '4.6.99'
+
+config = NetworkMapConfig
