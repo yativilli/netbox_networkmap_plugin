57576e9fa3e54345df5e2ae30b1cea0c6bf89a58
Yannick Wernle <yannick@wernle.net>
2026-08-28 10:37:13 +0200

    Introduced Color-coding by location

---
+++ network_map/colors.py
@@ -0,0 +1,26 @@
+LOCATION_COLORS = (
+    ("location-color-0", "#0072b2"),
+    ("location-color-1", "#d55e00"),
+    ("location-color-2", "#009e73"),
+    ("location-color-3", "#cc79a7"),
+    ("location-color-4", "#e69f00"),
+    ("location-color-5", "#56b4e9"),
+    ("location-color-6", "#882255"),
+    ("location-color-7", "#117733"),
+    ("location-color-8", "#332288"),
+    ("location-color-9", "#aa4499"),
+    ("location-color-10", "#44aa99"),
+    ("location-color-11", "#999933"),
+    ("location-color-12", "#661100"),
+    ("location-color-13", "#6699cc"),
+    ("location-color-14", "#aa4466"),
+    ("location-color-15", "#447744"),
+    ("location-color-16", "#774488"),
+    ("location-color-17", "#cc6677"),
+    ("location-color-18", "#117755"),
+    ("location-color-19", "#999933"),
+)
+
+
+def color_for_location(index: int) -> str:
+    return LOCATION_COLORS[index % len(LOCATION_COLORS)][0]
