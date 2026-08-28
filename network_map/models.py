195853d60cee91ff6705a890d5d3806c3684b850
Yannick Wernle <yannick@wernle.net>
2026-08-28 10:37:13 +0200

    Introduced Color-coding by location

--- network_map/models.py
+++ network_map/models.py
@@ -7,6 +7,9 @@ class NetworkElement(NetBoxModel):
     ip_address = models.GenericIPAddressField()
     device_type = models.CharField(max_length=50, choices=DeviceChoices)
     location = models.CharField(max_length=100, choices=LocationChoices)
+    role = models.CharField(max_length=50)
+    tags = models.CharField(max_length=200, blank=True)
+    color = models.CharField(max_length=20)
 
     def __str__(self):
         return f"{self.name} ({self.ip_address})"
