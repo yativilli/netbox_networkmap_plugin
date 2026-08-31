fda7588fc5998899f0a715bc634ea55e9e63d96f
Yannick Wernle <yannick@wernle.net>
2026-08-31 11:29:21 +0200

    Updated list with view for vlans with connected machines as prep for proper network-map

--- network_map/models.py
+++ network_map/models.py
@@ -13,3 +13,12 @@ class NetworkElement(NetBoxModel):
 
     def __str__(self):
         return f"{self.name} ({self.ip_address})"
+
+class VlanElement(NetBoxModel):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    group = models.CharField(max_length=100)
+    prefix = models.CharField(max_length=60)
+    status = models.CharField(max_length=20)
+    role = models.CharField(max_length=50)  
+    description = models.TextField(max_length=500, blank=True)
