4f27f8ca8b1456b1c527f2d05362b30b3d870018
Yannick Wernle <yannick@wernle.net>
2026-09-01 15:36:37 +0200

    Improving vlan-topology

--- network_map/urls.py
+++ network_map/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = (
+urlpatterns = [
     path('vlan-list/', views.VlanElementListView.as_view(), name='vlanelement_list'),
     path('topology/', views.NetworkElementTopologyView.as_view(), name='networkelement_topology'),
-)
+    path('vlan-topology/', views.VlanTopologyView.as_view(), name='vlan_topology'),
+]
