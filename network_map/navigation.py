ebef9a4336a291a49eb08d7852513c22640c3dc1
Yannick Wernle <yannick@wernle.net>
2026-09-01 13:37:34 +0200

    resolved errors, added navigation

---
+++ network_map/navigation.py
@@ -0,0 +1,14 @@
+from netbox.plugins import PluginMenuItem
+
+menu_items = (
+    PluginMenuItem(
+        link='plugins:network_map:vlanelement_list',
+        link_text='VLAN-Element Map',
+        permissions=['network_map.view_vlanelement'],
+    ),
+    PluginMenuItem(
+        link='plugins:network_map:networkelement_topology',
+        link_text='Network Topology Map',
+        permissions=['network_map.view_networkelement'],
+    ),
+)
