dd9f3417d81766f21116a6dfa29b7bc1f2c66615
Yannick Wernle <yannick@wernle.net>
2026-09-01 14:56:20 +0200

    started on topology-esque view

--- network_map/navigation.py
+++ network_map/navigation.py
@@ -7,6 +7,11 @@ menu_items = (
         permissions=['network_map.view_vlanelement'],
     ),
     PluginMenuItem(
+        link='plugins:network_map:vlan_topology',
+        link_text='VLAN Topology Map',
+        permissions=['network_map.view_vlanelement'],
+    ),
+    PluginMenuItem(
         link='plugins:network_map:networkelement_topology',
         link_text='Network Topology Map',
         permissions=['network_map.view_networkelement'],
