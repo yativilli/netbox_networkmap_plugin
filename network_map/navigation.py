d45d62539b47d6c1331ea4641ee8bbb4cb8afc0a
Yannick Wernle <yannick@wernle.net>
2026-09-03 08:47:42 +0200

    Removed NetworkElement and associated view

--- network_map/navigation.py
+++ network_map/navigation.py
@@ -3,17 +3,12 @@ from netbox.plugins import PluginMenuItem
 menu_items = (
     PluginMenuItem(
         link='plugins:network_map:vlanelement_list',
-        link_text='VLAN-Element Map',
+        link_text='Machine List',
         permissions=['network_map.view_vlanelement'],
     ),
     PluginMenuItem(
         link='plugins:network_map:vlan_topology',
-        link_text='VLAN Topology Map',
+        link_text='Topology Map',
         permissions=['network_map.view_vlanelement'],
-    ),
-    PluginMenuItem(
-        link='plugins:network_map:networkelement_topology',
-        link_text='Network Topology Map',
-        permissions=['network_map.view_networkelement'],
-    ),
+    )
 )
--- network_map/urls.py
+++ network_map/urls.py
@@ -4,6 +4,5 @@ from . import views
 
 urlpatterns = [
     path('vlan-list/', views.VlanElementListView.as_view(), name='vlanelement_list'),
-    path('topology/', views.NetworkElementTopologyView.as_view(), name='networkelement_topology'),
     path('vlan-topology/', views.VlanTopologyView.as_view(), name='vlan_topology'),
 ]
