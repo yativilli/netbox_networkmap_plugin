85058720146900ea5e3027cf7f1a02cbed47822f
Yannick Wernle <yannick@wernle.net>
2026-09-03 08:47:42 +0200

    Removed NetworkElement and associated view

--- network_map/views.py
+++ network_map/views.py
@@ -1,13 +1,11 @@
-import ipaddress
 
 from django.shortcuts import render
 from django.views import View
-from typing import List, Dict
 from dcim.models import Device
-from ipam.models import VLAN, IPAddress, Prefix
+from ipam.models import VLAN, IPAddress
 
-from .models import NetworkElement, VlanElement
-from .colors import color_for_location, location_color_map
+from .models import VlanElement
+from .colors import color_for_location
 
 
 class VlanElementListView(View):
@@ -127,51 +125,3 @@ class VlanTopologyView(VlanElementListView):
             'center_device': center_device,
         }
         return render(request, self.template_name, context)
-
-
-class NetworkElementTopologyView(View):
-    template_name = 'network_map/networkelement_topology.html'
-
-    def get_queryset(self):
-        return (Device.objects.filter(status="active", primary_ip4__isnull=False).select_related("device_type", "site", "primary_ip4", "role").order_by("site"))
-
-    def build_elements(self, queryset) -> List[NetworkElement]:
-        elements = []
-        for device in queryset:
-            ip = None
-            if device.primary_ip4:
-                ip = device.primary_ip4.address.ip
-
-            location = device.site.name if device.site else "None"
-
-            elements.append(NetworkElement(
-                name = device.name,
-                ip_address = ip,
-                device_type = device.device_type.model if device.device_type else "None",
-                location = location,
-                role = device.role.name if device.role else "None",
-                tags = [tag.name for tag in device.tags.all()],
-                color="location-color-default"
-            ))
-        return elements
-
-    def build_legend(self, elements: List[NetworkElement]) -> Dict[str, str]:
-        locations = sorted({element.location or "Unknown" for element in elements})
-        location_colors = location_color_map(locations)
-        legend = {}
-        for element in elements:
-            key = element.location or "Unknown"
-            element.color = location_colors.get(key, 'location-color-default')
-            legend[key] = element.color
-        return legend
-
-    def get(self, request):
-        queryset = self.get_queryset()
-        elements = self.build_elements(queryset)
-        legend = self.build_legend(elements)
-        context = {
-            'elements': elements,
-            'legend': legend,
-        }
-        return render(request, self.template_name, context)
-
