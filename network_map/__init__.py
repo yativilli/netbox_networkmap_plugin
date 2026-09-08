39d4c8c9dbf64d08c93f93b688eb032d0ebf0420
Yannick Wernle <yannick@wernle.net>
2026-09-08 11:17:14 +0200

    Restructuring of Module

--- network_map/__init__.py
+++ network_map/__init__.py
@@ -3,13 +3,14 @@ from netbox.plugins import PluginConfig
 class NetworkMapConfig(PluginConfig):
     name = "network_map"
     verbose_name = "Netbox Network Map"
-    description = "A simple network map integrated into netbox"
+    description = "A simple plugin displaying the relations of vlans in netbox"
     version = '0.1.0'
     base_url = 'networkmap'
     min_version = '4.5.0'
     max_version = '4.7.99'
     author = "Yannick Wernle"
     author_email = "yannick@wernle.net"
-    url = 'https://github.com/yativilli/netbox_networkmap_plugin'
+    license = 'GPL-3.0'   # <--- Make sure this is a string
+    author_url = 'https://github.com/yativilli/netbox_networkmap_plugin' 
 
 config = NetworkMapConfig
