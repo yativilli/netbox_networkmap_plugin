63a18cda9ebcdd51ad190b4eca84f8b7eccbbb94
Yannick Wernle <yannick@wernle.net>
2026-09-10 11:47:38 +0200

    Added pre-commit hook

--- network_map/navigation.py
+++ network_map/navigation.py
@@ -2,18 +2,18 @@ from netbox.plugins import PluginMenuItem
 
 menu_items = (
     PluginMenuItem(
-        link='plugins:network_map:vlanelement_list',
-        link_text='Machine List',
-        permissions=['network_map.view_vlanelement'],
+        link="plugins:network_map:vlanelement_list",
+        link_text="Machine List",
+        permissions=["network_map.view_vlanelement"],
     ),
     PluginMenuItem(
-        link='plugins:network_map:vlan_connections',
-        link_text='Logical Map',
-        permissions=['network_map.view_vlanelement'],
+        link="plugins:network_map:vlan_connections",
+        link_text="Logical Map",
+        permissions=["network_map.view_vlanelement"],
     ),
     PluginMenuItem(
-        link='plugins:network_map:vlan_topology',
-        link_text='Topology Map',
-        permissions=['network_map.view_vlanelement'],
-    )
+        link="plugins:network_map:vlan_topology",
+        link_text="Topology Map",
+        permissions=["network_map.view_vlanelement"],
+    ),
 )
